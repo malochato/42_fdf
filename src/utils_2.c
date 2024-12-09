eef411b11f640250a312072f4c06c566b17ac1d1
Malo <malo.dechateauvieux@gmail.com>
2024-12-10 07:06:30 +0800

    nonononorminettenononorminette

--- src/utils_2.c
+++ src/utils_2.c
@@ -6,7 +6,7 @@
 /*   By: malde-ch <malo@chato.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 15:51:17 by malde-ch          #+#    #+#             */
-/*   Updated: 2024/12/10 06:09:13 by malde-ch         ###   ########.fr       */
+/*   Updated: 2024/12/10 07:05:44 by malde-ch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,23 +50,21 @@ void	place_point(t_matrix *matrix, mlx_image_t *img, int x, int y)
 	set_pixel(img, matrix->map[x][y].proj_x, matrix->map[x][y].proj_y, 0xFFFFFFFF);
 }
 
-void connect_point(t_matrix *matrix, mlx_image_t *img, int x, int y)
+void	connect_point(t_matrix *matrix, mlx_image_t *img, int x, int y)
 {
-	if (x + 1 < matrix->nb_row) {
+	if (x + 1 < matrix->nb_row)
 		draw_bresenham_line(matrix->map[x][y], matrix->map[x + 1][y], img);
-	}
-	if (y + 1 < matrix->nb_col) {
+	if (y + 1 < matrix->nb_col)
 		draw_bresenham_line(matrix->map[x][y + 1], matrix->map[x][y], img);
-	}
 }
 
 
 
-void ft_isometric(t_matrix *matrix, mlx_image_t *img, int x, int y)
+void	ft_isometric(t_matrix *matrix, mlx_image_t *img, int x, int y)
 {
-	int temp_x = matrix->map[x][y].x;
-	int temp_y = matrix->map[x][y].y;
-	int temp_z = matrix->map[x][y].z;
+	int	temp_x = matrix->map[x][y].x;
+	int	temp_y = matrix->map[x][y].y;
+	int	temp_z = matrix->map[x][y].z;
 
 	double angle = 3.141592 / 6; // 30 degrés
 	int offset_x = 0;      // Décalage horizontal
@@ -85,7 +83,7 @@ void ft_isometric(t_matrix *matrix, mlx_image_t *img, int x, int y)
 }
 
 
-void ft_top(t_matrix *matrix, mlx_image_t *img, int x, int y)
+void	ft_top(t_matrix *matrix, mlx_image_t *img, int x, int y)
 {
 	matrix->map[x][y].proj_y = matrix->map[x][y].y;
 	matrix->map[x][y].proj_x = matrix->map[x][y].x;
@@ -95,11 +93,11 @@ void ft_top(t_matrix *matrix, mlx_image_t *img, int x, int y)
 
 
 
-void add_extrem_matrix(t_matrix *matrix, mlx_image_t *img, int x, int y)
+void	add_extrem_matrix(t_matrix *matrix, mlx_image_t *img, int x, int y)
 {
-	if(matrix->extremes.min_x < 0)
+	if (matrix->extremes.min_x < 0)
 		matrix->map[x][y].proj_x += abs(matrix->extremes.min_x);
-	if(matrix->extremes.min_y < 0)
+	if (matrix->extremes.min_y < 0)
 		matrix->map[x][y].proj_y += abs(matrix->extremes.min_y);
 	(void)img;
 }
